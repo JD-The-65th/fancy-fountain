ba6840ce450f62f75bf51edcac148e2a2be3bf3d
JD Williams <jd@void-team.com>
2026-05-25 16:54:52 -0400

    Nothing changed with Bottom Left Text

--- src/js/formatter/titlePage.js
+++ src/js/formatter/titlePage.js
@@ -29,15 +29,15 @@ export class titlePageFormatter extends formatter {
         document
             .fontSize(12)
             .font('Regular')
-            .text(bottomLeftText, 54, document.page.height - 80 - this.bottomLeftLineHeight, {
-                width: document.page.width - 54,
-                align: 'left', 
-            }
-            )
-            // Yes, I know this looks weird.
-            // Some people put notes on the title page and DON'T frame them as notes.
-            // FML.
-            .moveUp(16)
+
+        addEmphasizedText(document, bottomLeftText, 
+            {width: document.page.width - 54, align: 'left'}, 
+            [54, document.page.height - 80 - this.bottomLeftLineHeight])
+            
+        // Yes, I know this looks weird.
+        // Some people put notes on the title page and DON'T frame them as notes.
+        // FML.
+        document.moveUp(16)
     }
     addBottomRightText(document, bottomRightText) {
         this.bottomRightLineHeight += document.heightOfString(bottomRightText + "\n")
